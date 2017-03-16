150b344bc86fa6ebed027f37719e014b5d9ed083
Yang, Shanshan <SYang@vscatny.com>
2017-03-16 16:35:20 -0400

    added style

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PDF from './data/pdf';
+import './app.css';
 
 const PdfPageList = (props) => {
-  const pages = props.items.map(item => (
-    <li>
+  const pages = props.items.map((item, index) => (
+    <li key={index}>
       {decodeURIComponent(item.R[0].T)}
     </li>
   ));
@@ -11,10 +12,16 @@ const PdfPageList = (props) => {
   return <ul>{pages}</ul>;
 };
 
-const MyApp = () => (
-  <PdfPageList
-    items={PDF.formImage.Pages[0].Texts}
-  />
-);
+const MyApp = () => {
+  const containerStyle = {
+    width: `${PDF.formImage.Width}%`,
+  };
+
+  return (
+    <div style={containerStyle} className="wrapper">
+      <PdfPageList items={PDF.formImage.Pages[0].Texts} />
+    </div>
+  );
+};
 
 export default MyApp;
