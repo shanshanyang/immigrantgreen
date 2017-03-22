a7cdee07b05252154e4d49253f5c3a99d1066e5e
Yang, Shanshan <SYang@vscatny.com>
2017-03-22 12:56:56 -0400

    upload

--- src/MyApp.js
+++ src/MyApp.js
@@ -2,67 +2,26 @@ import React from 'react';
 import PDF from './data/pdf';
 import './app.css';
 
-// const pdfPageHlines = (props) => {
-  // const Hlines = props.items.map(item => (
-
-          // oc: '#221f1f',
-          // x: 2.442,
-          // y: 4.995,
-          // w: 9,
-          // l: 33.39,
-          
-  // ))
-// };
-// const pdfPageVlines = (props) => {
-            // oc: '#221f1f',
-            // x: 36,
-            // y: 9.546,
-            // w: 0.75,
-            // l: 7.551,
-          
-// };
-// const pdfPageFills = (props) => {
-            // oc: '#bfc0c3',
-            // x: 2.25,
-            // y: 9.546,
-            // w: 33.75,
-            // h: 1.673,
-            // clr: -1, //'clr' to reference a color with index in color dictionary
-          
-// };
-// const pdfPageTexts = (props) => {
-            // oc: '#221f1f',
-            // x: 32.16,
-            // y: 1.4740000000000002,
-            // w: 3.445,
-            // sw: 0.32553125,
-            // clr: -1,
-            // A: 'left',
-            // R: [
-            //   {
-            //     T: 'USCIS%20%20',
-            //     S: -1,
-            //     TS: [ //_kFontStyles dictionary 
-            //       0,
-            //       15,
-            //       1,
-            //       0,
-            //     ],
-            //   },
-            // ],
-// };
-// const pdfPage = (props) => {
-
-// };
-
 const PdfPageList = (props) => {
-  const pages = props.items.map((item, index) => (
-    <li key={index}>
+  const texts = props.items.Texts.map((item, index) => (
+    <div key={index}>
       {decodeURIComponent(item.R[0].T)}
-    </li>
+    </div>
   ));
+  // const canvas = document.createElement('canvas');
+  // canvas.id     = "CursorLayer";
+  // canvas.width  = 1224;
+  // canvas.height = 768;
+  // const context = canvas.getContext('2d');
+  // const lines = props.items.Hlines.map((item, index) => {
+  //   context.beginPath();
+  //   context.moveTo(0,0);
+  //   context.lineTo(item.x, item.y);
+  //   context.stroke();
+  // });
 
-  return <ul>{pages}</ul>;
+  // return <canvas width="100%" height=`${props.items.Height}%`>{texts}</canvas>;
+  return <div> {texts} </div>
 };
 
 const MyApp = () => {
@@ -72,7 +31,7 @@ const MyApp = () => {
 
   return (
     <div style={containerStyle} className="wrapper">
-      <PdfPageList items={PDF.formImage.Pages[0].Texts} />
+      <PdfPageList items={PDF.formImage.Pages[0]} />
     </div>
   );
 };
